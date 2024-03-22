9066cd30cc4d2fe20a8180bf5f3f839d5dfaccb7
santhoshn-vrize <Santhosh@vrize.com>
2024-03-22 16:00:49 +0530

    first commit

---
+++ src/main/java/com/codewithdurgesh/blog/BlogAppApis1Application.java
@@ -0,0 +1,13 @@
+package com.codewithdurgesh.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppApis1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppApis1Application.class, args);
+	}
+
+}
